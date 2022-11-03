c205835d532b33c70d7ffe529f493061f5ff8b97
Uday Kiran <56758575+udaycruise2903@users.noreply.github.com>
2022-11-03 15:21:31 +0530

    1-parse-json (#7)
    * added JSON instances
    
    * parse JSON and print DB
    
    * added jsonURL
    
    * removed sqlite db needs
    
    * removed SQLite code
    
    * satellites JSON parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,50 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Main where
 
+import Prelude hiding (id)
+import GHC.Generics                     
+import Data.Aeson                      
+import Database.SQLite.Simple           
+import Data.Text (Text,unpack)
+import Control.Applicative
+import Control.Monad
+import Network.HTTP.Conduit (simpleHttp)
+import qualified Data.ByteString.Lazy.Char8 as BC 
 import Brick (Widget, simpleMain, str)
 
-ui :: Widget ()
-ui = str "Hello World"
+-- The type received from JSON
+data DataPoint = DataPoint 
+          { id :: Text
+          , country :: Text
+          , launch_date :: Text
+          , mass :: Text
+          , launcher :: Text
+          } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON)
 
+data Satellites = Satellites 
+          { customer_satellites :: [DataPoint]
+          } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON )
+
+-- satellites api
+jsonURL :: String
+jsonURL = "https://isro.vercel.app/api/customer_satellites"
+
+getJSON :: IO BC.ByteString
+getJSON = simpleHttp jsonURL
+
+-- GUI
+ui :: Widget ()
+ui = str "Customer satellites launched by ISRO"
+
+-- parse the json, insert to DB, print entire DB
 main :: IO ()
-main = simpleMain ui
+main = do
+  simpleMain ui
+  s <- (eitherDecode <$> getJSON) :: IO (Either String Satellites)
+  case s of
+      Left err     -> putStrLn err
+      Right satellites -> print satellites
